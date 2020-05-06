18043f1333eb27dd8487af4eecdd240a971acae2
Neven Dyulgerov <neven.diulgerov@gmail.com>
2020-05-06 18:35:51 +0300

    Fix formatting

--- backend/src/database/models.py
+++ backend/src/database/models.py
@@ -13,6 +13,8 @@ db = SQLAlchemy()
 setup_db(app)
     binds a flask application and a SQLAlchemy service
 '''
+
+
 def setup_db(app):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -25,21 +27,26 @@ db_drop_and_create_all()
     can be used to initialize a clean database
     !!NOTE you can change the database_filename variable to have multiple verisons of a database
 '''
+
+
 def db_drop_and_create_all():
     db.drop_all()
     db.create_all()
+
 
 '''
 Drink
 a persistent drink entity, extends the base SQLAlchemy Model
 '''
+
+
 class Drink(db.Model):
-    # Autoincrementing, unique primary key
+    # Auto incrementing, unique primary key
     id = Column(Integer().with_variant(Integer, "sqlite"), primary_key=True)
     # String Title
     title = Column(String(80), unique=True)
     # the ingredients blob - this stores a lazy json blob
-    # the required datatype is [{'color': string, 'name':string, 'parts':number}]
+    # the required data type is [{'color': string, 'name':string, 'parts':number}]
     recipe = Column(String(180), nullable=False)
 
     '''
