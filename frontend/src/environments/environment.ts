c830a9ba8cc80370f84cf59897c3f3426b11cd45
Neven Dyulgerov <neven.diulgerov@gmail.com>
2020-05-07 19:49:02 +0300

    Update environment variables

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:8000', // the running FLASK api server url
   auth0: {
-    url: 'neatsu.auth0.com', // the auth0 domain prefix
-    audience: 'image', // the audience set for the auth0 app
-    clientId: '7DcSDe5KHGbofMl4N6j9I3V6me4ac3Eg', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8080/login-results' // http://localhost:8100', // the base url of the running ionic application.
+    url: 'neatsu', // the auth0 domain prefix
+    audience: 'coffee-shop', // the audience set for the auth0 app
+    clientId: 'IEn9rCg6HxdcPcV3po79RevldEFtbjan', // the client id generated for the auth0 app
+    callbackURL: 'http://localhost:4200', // the base url of the running ionic application.
   }
 };
